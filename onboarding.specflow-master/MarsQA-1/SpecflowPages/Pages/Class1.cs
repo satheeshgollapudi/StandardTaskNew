343ca975f9c205b9feda3e4b8b5e3d835d0d567f
satheesh gollapudi <gskumar3107@gmail.com>
2025-01-28 10:25:04 +1300

    c

--- onboarding.specflow-master/MarsQA-1/SpecflowPages/Pages/Class1.cs
+++ onboarding.specflow-master/MarsQA-1/SpecflowPages/Pages/Class1.cs
@@ -9,11 +9,6 @@ namespace MarsQA_1.SpecflowPages.Pages
     internal class Class1
     {
        //demo1
-<<<<<<< HEAD
-       //line2
-=======
-       //demo2
->>>>>>> branch2
-
+        //demo2  //line2
     }
 }
